63b11f7723070db7528fafa1a1604c4dc9100526
brdevs-tm <bright19.off@gmail.com>
2023-09-20 08:14:10 +0500

    third commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 const user = document.querySelector(".user");
 const userDropdown = document.querySelector(".user-dropdown");
 const numberInput = document.querySelector("#number-input");
-const formSubmit = document.querySelector(".form-submit");
-const studentEditForm = document.querySelector(".student-edit-form");
+const teacherEditModal = document.querySelector(".edit-teacher-modal");
+const studentEditModal = document.querySelector(".edit-student-modal");
+const teacherAddModal = document.querySelector(".add-teacher-modal");
+const studentAddModal = document.querySelector(".add-student-modal");
+
+const formSubmitBtn = document.querySelector(".form-submit");
+const editStudentButton = document.querySelectorAll(".edit-student");
+const editTeacherButton = document.querySelectorAll(".edit-teacher");
+const addStudentBtn = document.querySelectorAll(".add-student");
+// const addTeacherBtn = document.querySelector(".add-teacher");
 
 user.addEventListener("click", (event) => {
   event.stopPropagation();
@@ -19,6 +27,24 @@ window.addEventListener("click", () => {
   }
 });
 
+editStudentButton.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    studentEditModal.classList.add("show-modal");
+  });
+});
+
+editTeacherButton.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    teacherEditModal.classList.add("show-modal");
+  });
+});
+
+addStudentBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    studentAddModal.classList.add("show-modal");
+  });
+});
+
 /* Phone Number REGEX */
 
 function validateUzbekistanPhoneNumber(phoneNumber) {
@@ -27,11 +53,10 @@ function validateUzbekistanPhoneNumber(phoneNumber) {
   return uzbekistanPhoneNumberRegex.test(phoneNumber);
 }
 
-formSubmit.addEventListener("click", (event) => {
-  event.preventDefault();
-
+formSubmitBtn.addEventListener("click", () => {
   const phoneNumberValue = numberInput.value;
   if (validateUzbekistanPhoneNumber(phoneNumberValue)) {
+    studentEditForm.classList.remove("show-modal");
   } else {
     alert(
       `Error: phone number! If you find an error in any mode, send a pic of error to my telegram It's my telegram account: "https//t.me/br_power"`
